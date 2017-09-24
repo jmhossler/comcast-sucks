a12e06be60c9304e882ed62382138f0b2ebf9137
John Hossler <john.m.hossler@gmail.com>
2017-09-24 21:04:55 +0000

    Added the tool to get twitter API object

--- comcast_sucks/_utilities.py
+++ comcast_sucks/_utilities.py
@@ -1,4 +1,6 @@
+import os
 import requests
+import twitter
 
 
 def internetUp():
@@ -8,3 +10,12 @@ def internetUp():
     except requests.ConnectionError:
 	pass
     return False
+
+
+def getApi():
+    c = os.getenv('CONSUMER_KEY')
+    c_s = os.getenv('CONSUMER_SECRET')
+    a_t = os.getenv('ACCESS_TOKEN')
+    a_t_s = os.getenv('ACCESS_TOKEN_SECRET')
+    return twitter.Api(consumer_key=c, consumer_secret=c_s,
+                       access_token_key=a_t, access_token_secret=a_t_s)
